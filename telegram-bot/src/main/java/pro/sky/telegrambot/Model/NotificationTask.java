8815d6813e43d1dcdd5dc34d932c614884ae26c3
Anikeev Igor <415b@mail.ru>
2023-01-26 09:32:32 +0300

    fix

--- telegram-bot/src/main/java/pro/sky/telegrambot/Model/NotificationTask.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/Model/NotificationTask.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 public class NotificationTask {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long chatId;
--- telegram-bot/src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -7,7 +7,6 @@ import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import pro.sky.telegrambot.Model.NotificationTask;
@@ -25,13 +24,17 @@ import java.util.regex.Pattern;
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
-    @Autowired
-    private TelegramBot telegramBot;
 
-    @Autowired
-    private NotificationRepo notificationRepo;
+    private final TelegramBot telegramBot;
+
+    private final NotificationRepo notificationRepo;
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot, NotificationRepo notificationRepo) {
+        this.telegramBot = telegramBot;
+        this.notificationRepo = notificationRepo;
+    }
 
     @PostConstruct
     public void init() {
@@ -41,43 +44,41 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Scheduled(cron = "0 0/1 * * * *")
     public void sendMessageInTime() {
-       List<NotificationTask> notiss = new ArrayList<>(notificationRepo.getNotificationTaskByNotificationDate(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES)));
-       for(NotificationTask notificationTask:notiss){
-           SendMessage message = new SendMessage(notificationTask.getChatId(),notificationTask.getNotificationText());
-           telegramBot.execute(message);
-       }
+        List<NotificationTask> notiss = new ArrayList<>(notificationRepo.getNotificationTaskByNotificationDate(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES)));
+        for (NotificationTask notificationTask : notiss) {
+            SendMessage message = new SendMessage(notificationTask.getChatId(), notificationTask.getNotificationText());
+            telegramBot.execute(message);
+        }
     }
 
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-            Long chatId = update.message().chat().id();
-            String inMessText = update.message().text();
+                    logger.info("Processing update: {}", update);
+                    Long chatId = update.message().chat().id();
+                    String inMessText = update.message().text();
 
-            SendMessage outMess = new SendMessage(chatId,"Hi");
-            SendMessage outMess1 = new SendMessage(chatId,"Unknown command");
-
-            if(inMessText.equals("/start")){
-            SendResponse response = telegramBot.execute(outMess);
-            } else {
-                SendResponse response = telegramBot.execute(outMess1);
-            }
-
-            Pattern pattern = Pattern.compile("([0-9\.\:\s]{16})(\s)([\W+]+)");
-            Matcher matcher = pattern.matcher(inMessText);
-            if(matcher.matches()){
-                String date =matcher.group(1);
-                String text =matcher.group(3);
-                NotificationTask notificationTask = new NotificationTask();
-                notificationTask.setChatId(chatId);
-                notificationTask.setNotificationDate(LocalDateTime.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")));
-                notificationTask.setNotificationText(text);
-                notificationRepo.save(notificationTask);
-            }
+                    if (inMessText.equals("/start")) {
+                        SendMessage outMess = new SendMessage(chatId, "Hi");
+                        SendResponse response = telegramBot.execute(outMess);
+                    }
+                    final Pattern pattern = Pattern.compile("([0-9.:\\s]{16})(\\s)([\\W+]+)");
+                    final Matcher matcher = pattern.matcher(inMessText);
+                    if (matcher.matches()) {
+                        String date = matcher.group(1);
+                        String text = matcher.group(3);
+                        NotificationTask notificationTask = new NotificationTask();
+                        notificationTask.setChatId(chatId);
+                        notificationTask.setNotificationDate(LocalDateTime.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")));
+                        notificationTask.setNotificationText(text);
+                        notificationRepo.save(notificationTask);
+                        SendMessage outMess2 = new SendMessage(chatId, "notification was add");
+                        SendResponse response1 = telegramBot.execute(outMess2);
+                    } else {
+                        SendMessage outMess1 = new SendMessage(chatId, "Unknown command");
+                        SendResponse response2 = telegramBot.execute(outMess1);
+                    }
                 }
-
-             // Process your updates here
         );
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
